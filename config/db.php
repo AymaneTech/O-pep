7b66e9cdbf4c926b39562c28c6a0ceb449d3a6b3
Ayamne WSL <elmainiaymane03@gmail.com>
2023-11-22 01:23:07 +0100

    https://aymaneelmaini.atlassian.net/browse/OB-15 i connect php to my db

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+$dsn = "mysql:host=localhost; dbname=opep";
+$username= "root";
+$pd="";
+try {
+    $pdo = new PDO($dsn, $username, $pd);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo "connection to db failed homie !!! " . $e->getMessage();
+}
