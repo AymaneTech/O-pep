7f4f70a25e8475b27c654dbb49e230480dc24ec5
Ayamne WSL <elmainiaymane03@gmail.com>
2023-11-28 16:02:29 +0100

    i finish the inserting of plant to the cart of that user

--- models/addtoCart.php
+++ models/addtoCart.php
@@ -12,21 +12,25 @@ function checkCartExisting($user_id){
     $stmt = $pdo->prepare($query);
     $stmt->bindParam("user_id", $user_id, PDO::PARAM_STR);
     $stmt->execute();
-    $cart_id = $stmt->fetch(PDO::FETCH_COLUMN); // Use FETCH_COLUMN to retrieve a single value
+    $cart_id = $stmt->fetch(PDO::FETCH_COLUMN);
     return $cart_id;
 }
 // we use this cart when the client already have a cart
-function create_cartPlant($cart_id,$plant_id){
+function already_have_cart($cart_id,$plant_id){
     global $pdo;
     $query = "INSERT INTO cart_plant (cart_id, plant_id) VALUES (:cart_id, :plant_id);";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':cart_id', $cart_id, PDO::PARAM_INT);
     $stmt->bindParam(':plant_id', $plant_id, PDO::PARAM_INT);
-    $stmt->execute();
+    if($stmt->execute()){
+        echo "i insert by already_have acart";
+    }else{
+        echo "query not executed";
+    }
 }
 
 // and this function work when the user have no cart we create for him a cart
-function createCartForUser(){
+function create_cart_for_user(){
     try {
         global $pdo;
         $plant_id = (int)$_GET["plant_id"];
@@ -41,18 +45,14 @@ function createCartForUser(){
     }
 
 }
-
 // and this is the function that decide wich function will work depending the result of check car existing function
-function add_plant_to_cart($user_id, $plant_id){
+function main_fuction($user_id, $plant_id){
     $cart_id = checkCartExisting($user_id);
     if ($cart_id > 0) {
-        echo "I already have a cart ";
-        create_cartPlant($cart_id, $plant_id);
+        already_have_cart($cart_id, $plant_id);
     } else {
-        echo "I have no cart ";
-        createCartForUser($plant_id);
+        create_cart_for_user();
     }
+    header("location: ../template?status=added successfly to cart");
 }
-
-add_plant_to_cart($user_id, $plant_id);
-add_plant_to_cart($user_id);
+main_fuction($user_id, $plant_id);
